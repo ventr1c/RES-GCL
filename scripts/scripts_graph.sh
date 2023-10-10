ecca2bc80cb6efce8fc5c33352a50d00b72f37b6
ventr1c <mfl5681@psu.edu>
2023-10-10 12:06:18 -0400

    clean duplicate codes

--- scripts/scripts_graph.sh
+++ scripts/scripts_graph.sh
@@ -1,3 +1,3 @@
-nohup python -u run_certify_graph.py --dataset ogbg-molhiv --encoder_model BGRL-G2L --device_id 0 --num_repeat 1 --n0 20 --n 200 --alpha 0.01 --batch_size 1024
-nohup python -u run_certify_graph.py --dataset ogbg-molhiv --encoder_model GraphCL --device_id 1 --num_repeat 1 --n0 50 --n 200 --alpha 0.01 --batch_size 1024 
+python -u run_certify_graph.py --dataset ogbg-molhiv --encoder_model BGRL-G2L --device_id 0 --num_repeat 1 --n0 20 --n 200 --alpha 0.01 --batch_size 1024
+python -u run_certify_graph.py --dataset ogbg-molhiv --encoder_model GraphCL --device_id 1 --num_repeat 1 --n0 50 --n 200 --alpha 0.01 --batch_size 1024 
 
